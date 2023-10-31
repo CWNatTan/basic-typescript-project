f5dea48544d504c7e475af96a46643c7f34518e4
Nathanael Tan <nathanael.tan@coxautoinc.com>
2023-10-31 10:14:58 +0000

    Wack
    yes

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,16 +1,46 @@
-import { cookingTime } from './index';
+import { cookingTime, likes } from './index';
 import assert = require('assert');
 
 describe("Egg Tests", () => {
-    function doEggTest(eggs: number, time: number): void {
+    function doTest(eggs: number, time: number): void {
         expect(cookingTime(eggs)).toBe(time);
     }
 
     test("0 Eggs", () => {
-        doEggTest(0, 0);
-        doEggTest(3, 5);
-        doEggTest(10, 10);
-        doEggTest(16, 10);
-        doEggTest(32, 20);
+        doTest(0, 0);
+        doTest(3, 5);
+        doTest(10, 10);
+        doTest(16, 10);
+        doTest(32, 20);
+    })
+})
+
+describe("Likes Test", () => {
+    function doTest(people: string[], output: string): void {
+        expect(likes(people)).toBe(output);
+    }
+
+    test("No People", () => {
+        doTest([], 'no one likes this');
+    })
+
+    test("1 Person", () => {
+        doTest(["Peter"], 'Peter likes this');
+    })
+
+    test("2 People", () => {
+        doTest(["Peter", "Alex"], 'Peter and Alex like this');
+    })
+
+    test("3 People", () => {
+        doTest(["Peter", "Michael", "Alex"], 'Peter, Michael and Alex like this');
+    })
+
+    test("More", () => {
+        doTest(["Peter", "Michael", "Alex", "Jacob"], 'Peter, Michael and 2 others like this');
+    })
+
+    test("Much More", () => {
+        doTest(["Peter", "Michael", "Alex", "Jacob", "Michael", "Alex", "Jacob"], 'Peter, Michael and 5 others like this');
     })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 export function cookingTime(eggs: number): number {
     return Math.ceil(eggs / 8) * 5;
 }
+
+export const likes = (a: string[]): string => {
+    return [
+        "no one likes this",
+        `${a[0]} likes this`,
+        `${a[0]} and ${a[1]} like this`,
+        `${a[0]}, ${a[1]} and ${a[2]} like this`,
+        `${a[0]}, ${a[1]} and ${a.length - 2} others like this`,
+    ][Math.min(a.length, 4)];
+}
